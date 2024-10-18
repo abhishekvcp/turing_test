1e4641214d3b5d5ced1ec93401adc20417358c80
Abhishek Panakkaran <abhishek.panakkaran@gmail.com>
2024-10-18 10:41:58 +0530

    fix : new parameter

--- project1/test_project1.py
+++ project1/test_project1.py
@@ -4,8 +4,8 @@ from words import solutions
 class TestMyFile(unittest.TestCase):
     def test_words_solutions(self):
         self.assertEqual(solutions(["good","god"]), 1)
-        self.assertEqual(solutions(["car","carpet","torch","run","running"]), 2)
-        self.assertEqual(solutions(["good","gtt"]), 1)
+        self.assertEqual(solutions(["car","carpet","torch","run","runn"]), 1)
+        self.assertEqual(solutions(["good","gtt"]), 0)
 
 
 if __name__ == "__main__":
